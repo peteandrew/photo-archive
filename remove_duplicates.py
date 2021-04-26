356b75b264bc58a6ffbd367870be0b9ed08ed4ac
Pete Andrew <peteandrew101@gmail.com>
2021-04-26 17:15:41 +0100

    Added script to remove duplicate DynamoDB items. Check for existing items for files before adding new items

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -18,12 +18,9 @@ cur = db.execute('SELECT filename, time_created, time_processed, original_direct
 for row in cur:
     print(row)
 
-    image_id = str(uuid.uuid4())
     filename = row[0]
     time_created = row[1]
-    created_year_month = row[1][:7]
 
-    # Find any existing items for file
     items = []
     last_evaluated_key = None
     continue_scan = True
@@ -53,36 +50,15 @@ for row in cur:
         except botocore.exceptions.ClientError as e:
             print(e)
 
-    if len(items) > 0:
-        print("Item already exists for file, skipping")
-        continue
-
-    try:
-        response = client.put_item(
-            TableName='IA_Images',
-            Item={
-                'ImageID': {
-                    'S': image_id
-                },
-                'CreatedYearMonth': {
-                    'S': created_year_month
-                },
-                'TimeCreated': {
-                    'S': row[1]
-                },
-                'CurrentFilename': {
-                    'S': row[0]
-                },
-                'TimeProcessed': {
-                    'S': row[2]
-                },
-                'OriginalDirectory': {
-                    'S': row[3]
-                }
-            }
-        )
-        print(response)
-    except botocore.exceptions.ClientError as e:
-        print(e)
+    if len(items) > 1:
+        print(len(items))
+        for item in items:
+            if 'GlacierState' not in item:
+                client.delete_item(
+                    TableName='IA_Images',
+                    Key={
+                        'ImageID': item['ImageID']
+                    }
+                )
 
 cur.close()
